c46f929e13434db50fa4b0026d32712c9593ef82
pollchihuy <poll.chihuy@gmail.com>
2022-09-04 18:43:53 +0700

    First Deploy

---
+++ asyncexample/src/main/java/asyncexample/asyncsimple/MyThreadFirst.java
@@ -0,0 +1,21 @@
+package asyncexample.asyncsimple;
+
+public class MyThreadFirst extends Thread{
+
+	public void run() {
+	    try {
+	      for (int i=0; i<10; i++) {
+	        System.out.println("First Thread in " +(i+1)+" second ");
+	        if (i != 9) {
+	        	sleep(2000);
+	        } else {
+	          System.out.println("First Thread finish !!...\n");
+	        }
+	      }
+	    } 
+	    catch (InterruptedException ie) 
+	    {
+	      System.out.println(ie.getMessage());
+	    }
+	  }
+}
