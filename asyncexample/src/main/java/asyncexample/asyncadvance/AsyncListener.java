26a45cae81f278f2f6dc3f4e06bc3776353fcb3b
Rahmad Nur Azim <rahmad.azim@aux.dkatalis.com>
2022-11-10 09:42:53 +0700

    Add initaliz

--- asyncexample/src/main/java/asyncexample/asyncadvance/AsyncListener.java
+++
@@ -1,9 +0,0 @@
-package asyncexample.asyncadvance;
-
-public interface AsyncListener {
-	
-		  
-	    // this can be any type of method
-	    public void onAsyncParent(int intAsyncParent);
-	    public void onAsyncChild(int intAsyncParent,int intAsyncParentLoop);
-}
--- asyncexample/src/main/java/asyncexample/asyncdeploy/DoneToDeploy.java
+++
@@ -1,7 +0,0 @@
-package asyncexample.asyncdeploy;
-
-public class DoneToDeploy {
-	public static void main(String[] args) {
-		System.out.println("DONE !!!!!");
-	}
-}
